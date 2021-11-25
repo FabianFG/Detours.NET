9f34b07bdb25e6b9666e60dc39b2a77474577ac8
Fabian <mcdtloeser@gmail.com>
2021-11-25 13:29:37 +0100

    Add project files.

---
+++ Detours.NET/Detours.NET.h
@@ -0,0 +1,13 @@
+#pragma once
+
+using namespace System;
+
+namespace Detours
+{
+	namespace NET {
+		public ref class Detours abstract sealed
+		{
+			// TODO: Add your methods for this class here.
+		};
+	}
+}
