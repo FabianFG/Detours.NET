e8de51f28fe5ff7f67a7a89bde63ad80428877bb
Fabian <mcdtloeser@gmail.com>
2021-12-11 16:17:23 +0100

    add an interface for detours hook that isn't generic

---
+++ Detours.NET/IDetoursHook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DetoursNET
+{
+    public interface IDetoursHook : IDisposable
+    {
+        public unsafe void* OriginalPtr { get; }
+        public unsafe void* OriginalFuncPtr { get; }
+        public unsafe void* DetourPtr { get; }
+    }
+}
